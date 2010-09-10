8c6dc78b7ab3244671b3dcafacf96df3d587916b
Henrique <hgtesta@gmail.com>
2010-09-10 19:26:26 -0300

    Added stylesheet

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   def breadcrumb(*args)
     navigation = {
-      :panels        => lambda { link_to "Panels", panels_path }
+      :panels        => lambda { link_to "Panels", panels_path },
       :current_panel => lambda { link_to @panel.name, panel_path(@panel) }
     }
     links = args[0..-2].map { |place| navigation[place] }.join " | "
