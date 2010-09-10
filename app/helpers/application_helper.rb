c8c6b23a358449b117ed2b373061829fbbe0fcd9
Henrique <hgtesta@gmail.com>
2010-09-10 15:22:05 -0300

    Created breadcrumb helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+  def breadcrumb(*args)
+    navigation = {
+      :panels        => lambda { link_to "Panels", panels_path }
+      :current_panel => lambda { link_to @panel.name, panel_path(@panel) }
+    }
+    links = args[0..-2].map { |place| navigation[place] }.join " | "
+    %{
+      <div class="breadcrumb">
+        #{links}
+      </div>
+    }
+  end
+
 end
